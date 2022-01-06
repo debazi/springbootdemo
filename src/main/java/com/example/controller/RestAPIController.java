a3cbdb73e4dcc0fbe63f49bea84befc26557f66d
debazi <marcdebazi@gmail.com>
2022-01-06 15:06:21 +0000

    make it better

--- src/main/java/com/example/controller/RestAPIController.java
+++ src/main/java/com/example/controller/RestAPIController.java
@@ -1,18 +1,20 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author marc-assie
  */
+@RequestMapping("/api")
 @RestController
 public class RestAPIController {
 
     /**
      * @return xxxxxxxxxxxxxxx
      */
-    @RequestMapping("/")
+    @GetMapping("/hello")
     public String findAll () {
         return "Hello World!";
     }
